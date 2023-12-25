80d4303ff59d63a8022d213afc053cb6953de98b
nelsonelaye <nelsonelaye@gmail.com>
2023-12-25 12:31:44 +0100

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+// this is the required root layout for the entire application
+//use it to modify your <html> and <body> tags, and add metadata
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
